622aa89938cb0bbf85b07edd2ae11b7ba0101211
yotti5160 <40465007+yotti5160@users.noreply.github.com>
2018-08-16 15:02:43 +0800

    Create 401BinaryWatch.py

---
+++ 401BinaryWatch.py
@@ -0,0 +1,29 @@
+class Solution:
+    def readBinaryWatch(self, num):
+        """
+        :type num: int
+        :rtype: List[str]
+        """
+        def hour(input):
+            r=[]
+            for i in range(12):
+                if bin(i).count('1')==input:
+                    r.append(str(i)+':')
+            return r
+        def minute(input):
+            r=[]
+            for i in range(60):
+                if bin(i).count('1')==input:
+                    r.append(str(i).zfill(2))
+            return r
+
+        result=[]
+        for i in range(min(5,num+1)):
+            tmpHour=hour(i)
+            tmpMin=minute(num-i)
+            for h in tmpHour:
+                for m in tmpMin:
+                    result.append(h+m)
+        return result
+
+print(Solution.readBinaryWatch(Solution, 2))
