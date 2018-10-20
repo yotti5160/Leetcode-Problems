33a35ac0dbad22965bb67f01c676e6a2dbe4fed2
yotti5160 <40465007+yotti5160@users.noreply.github.com>
2018-10-20 13:43:02 +0800

    Create 322_Coin Change.py

---
+++ 322_Coin Change.py
@@ -0,0 +1,19 @@
+class Solution:
+    def coinChange(self, coins, amount):
+        
+        def dfs(amount, nowCount, startIndex):
+            if nowCount+math.ceil(amount/coins[startIndex])>=self.ret:
+                return
+            if amount%coins[startIndex]==0:
+                self.ret=min(self.ret, nowCount+amount//coins[startIndex])
+            if startIndex==l-1:
+                return
+            for i in range(amount//coins[startIndex], -1, -1):
+                dfs(amount-i*coins[startIndex], nowCount+i, startIndex+1)
+            return
+                
+        self.ret=amount+1
+        l=len(coins)
+        coins.sort(reverse=True)
+        dfs(amount, 0, 0)
+        return self.ret if self.ret<(amount+1) else -1
